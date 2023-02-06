1c4bf9e05d381a4b9d313cb9e5ef81b04a4401da
CorentinJGosling <you@example.com>
2023-02-06 22:33:45 +0100

    police issues

--- js/script.js
+++ js/script.js
@@ -206,25 +206,6 @@ menuItems.forEach(function (menuItem) {
   menuItem.addEventListener("click", toggleMenu);
 });
 
-// window.onload = function () {
-//   document
-//     .getElementById("contact-form")
-//     .addEventListener("submit", function (event) {
-//       event.preventDefault();
-//       // generate a five digit number for the contact_number variable
-//       this.contact_number.value = (Math.random() * 100000) | 0;
-//       // these IDs from the previous steps
-//       emailjs.sendForm("contact_service", "contact_form", this).then(
-//         function () {
-//           console.log("SUCCESS!");
-//         },
-//         function (error) {
-//           console.log("FAILED...", error);
-//         }
-//       );
-//     });
-// };
-
 function SendMail() {
   var params = {
     title: document.getElementById("title").value,

06f9209cf9691e1c89b5b8ace1cd656d5c7718cc
CorentinJGosling <you@example.com>
2023-02-16 15:09:49 +0100

    16022023

--- js/script.js
+++ js/script.js
@@ -1,144 +1,9 @@
-// SELECT CAROUSEL
-const carousel = document.querySelector(".carousel");
-
-// SELECT NEXT BUTTON
-const nextButton = document.querySelector(".right-btn");
-
-// SELECT LEFT BUTTON
-const previousButton = document.querySelector(".left-btn");
-
-// SELECT THE NAV
-const nav = document.querySelector(".nav");
-
-// SELECT THE TEXT
-const explain = document.querySelector(".explain");
-
-// SELECT ALL THE EXPLANATIONS
-const expTxt = [...explain.children];
-
-// SELECT ALL THE DOTS
-const dots = [...nav.children];
-
-// SELECT ALL THE SLIDES INSIDE THE CAROUSEL
-const slides = [...carousel.children];
-
-// CALCULATE THE SLIDE WIDTH
-let slideWidth = slides[0].getBoundingClientRect().width;
-
-// POSITION THE SLIDES HORIZONTALY
-function positionSlides(slides) {
-  for (let index = 0; index < slides.length; index++) {
-    slides[index].style.left = slideWidth * index + "px";
-  }
-}
-
-positionSlides(slides);
-
-// ON RIGHT BUTTON CLICK, WE MOVE(TranslateX) THE CAROUSEL TO THE LEFT
-nextButton.addEventListener("click", function () {
-  const currentSlide = carousel.querySelector(".active");
-  const nextSlide = currentSlide.nextElementSibling;
-
-  moveToSlide(carousel, currentSlide, nextSlide);
-  hideButton(nextSlide, slides);
-  moveToDot(nextSlide, slides, nav, dots);
-  moveToTxT(nextSlide, slides, explain, expTxt);
+new Typed("#typed", {
+  strings: ["scientific writing", "data analysis"],
+  typeSpeed: 80,
+  delaySpeed: 90,
+  loop: true,
 });
-
-// ON LEFT BUTTON CLICK, WE MOVE(TranslateX) THE CAROUSEL TO THE RIGHT
-previousButton.addEventListener("click", function () {
-  const currentSlide = carousel.querySelector(".active");
-  const previousSlide = currentSlide.previousElementSibling;
-
-  moveToSlide(carousel, currentSlide, previousSlide);
-  hideButton(previousSlide, slides);
-  moveToDot(previousSlide, slides, nav, dots);
-  moveToTxT(previousSlide, slides, explain, expTxt);
-});
-
-// ON DOT CLICK
-nav.addEventListener("click", function (e) {
-  // if we didn't click on a dot, we exit
-  if (e.target === nav) return;
-
-  // SELECT THE CLICKED DOT
-  const targetDot = e.target;
-
-  // SELECT THE CURRENT DOT
-  const currentDot = nav.querySelector(".active");
-
-  // SELECT THE CURRENT SLIDE
-  const currentSlide = carousel.querySelector(".active");
-
-  // SELECT THE CURRENT Txt
-  const currentTxt = explain.querySelector(".active");
-
-  // find the index of the dot, so we can target the right slide
-  let targetDotIndex = findIndex(targetDot, dots);
-
-  // SELECT THE TARGET SLIDE
-  const targetSlide = slides[targetDotIndex];
-
-  moveToSlide(carousel, currentSlide, targetSlide);
-  toggleActive(currentDot, targetDot);
-  moveToTxT(targetSlide, slides, explain, expTxt);
-  hideButton(targetSlide, slides);
-});
-
-// MOVE TO DOT
-function moveToDot(targetSlide, slides, nav, dots) {
-  let slideIndex = findIndex(targetSlide, slides);
-  const currentDot = nav.querySelector(".active");
-  const targetDot = dots[slideIndex];
-  toggleActive(currentDot, targetDot);
-}
-// MOVE TO TXT
-function moveToTxT(targetSlide, slides, explain, expTxt) {
-  let slideIndexTxT = findIndex(targetSlide, slides);
-  const currentTxt = explain.querySelector(".active");
-  const targetTxt = expTxt[slideIndexTxT];
-  toggleActive(currentTxt, targetTxt);
-}
-// MOVE TO SLIDE
-function moveToSlide(carousel, currentSlide, targetSlide) {
-  const position = targetSlide.style.left;
-  carousel.style.transform = `translateX(-${position})`;
-  toggleActive(currentSlide, targetSlide);
-}
-
-// Toggle ACTIVE CLASS
-function toggleActive(current, target) {
-  current.classList.remove("active");
-  target.classList.add("active");
-}
-
-// HIDE BUTTON
-function hideButton(targetSlide, slides) {
-  // If the target slide is the first slide the previous button must be hidden
-  // and the next button must be shown
-  if (targetSlide === slides[0]) {
-    previousButton.classList.add("hide");
-    nextButton.classList.remove("hide");
-  } else if (targetSlide === slides[slides.length - 1]) {
-    // If the target slide is the last slide the next button must be hidden
-    // and the previous button must be shown
-    nextButton.classList.add("hide");
-    previousButton.classList.remove("hide");
-  } else {
-    // if none of the above is true, we show both the next and prevoius button
-    previousButton.classList.remove("hide");
-    nextButton.classList.remove("hide");
-  }
-}
-
-// FIND THE INDEX OF AN ITEM, INSIDE AN ARRAY OF ITEMS
-function findIndex(item, items) {
-  for (let index = 0; index < items.length; index++) {
-    if (item === items[index]) {
-      return index;
-    }
-  }
-}
 
 let modalBtn1 = document.getElementById("modal-btn1");
 let modal1 = document.querySelector(".modal1");
@@ -206,19 +71,39 @@ menuItems.forEach(function (menuItem) {
   menuItem.addEventListener("click", toggleMenu);
 });
 
+function CheckMail() {
+  var boxID = document.getElementById("form_id");
+  var NameID = document.getElementById("fullName").value;
+  var emailID = document.getElementById("email_id").value;
+  var messID = document.getElementById("message").value;
+  var budgetID = document.getElementById("budget_id").value;
+  const fail = document.querySelector(".formFail");
+  const succ = document.querySelector(".formSent");
+
+  if (NameID == "" || emailID == "" || messID == "" || budgetID == "") {
+    fail.style.display = "block";
+    succ.style.display = "none";
+  } else {
+    SendMail();
+    boxID.reset();
+    succ.style.display = "block";
+    fail.style.display = "none";
+  }
+}
 function SendMail() {
   var params = {
     title: document.getElementById("title").value,
     from_name: document.getElementById("fullName").value,
     email_id: document.getElementById("email_id").value,
     message: document.getElementById("message").value,
+    budget_id: document.getElementById("budget_id").value,
   };
   emailjs
     .send("service_mhgccsp", "template_q68z7ym", params)
     .then(function (res) {
       if (res.status == 200) {
-        mess =
-          "Your message has been sent! You will hear from us in the coming days.";
+        // mess =
+        //   "Your message has been sent! You will hear from us in the coming days.";
       } else {
         mess =
           "Your message has not been sent! Please send you inquiry directly by email at x@x.";
@@ -227,14 +112,6 @@ function SendMail() {
     });
 }
 
-new Typed("#typed", {
-  strings: ["scientific writing", "data analysis"],
-  typeSpeed: 80,
-  delaySpeed: 90,
-  loop: true,
-});
-// ScrollReveal().reveal(".sec2head", { delay: 1000 });
-// ScrollReveal().reveal(".sec2row", { delay: 1000 });
 ScrollReveal().reveal(".member", {
   delay: 300,
   distance: "100%",
